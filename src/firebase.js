28afa5be0628b8b2e1294cc60e3ca4fa171ec8c9
NetsanetAlemu <netsialemu1907@gmail.com>
2024-03-15 12:55:20 +0300

    updated credential visibility

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
-import {getStorage} from 'firebase/storage';
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD76tw1MYdadwFeO2cLtkCiH34Bqe6heaI",
-  authDomain: "uploadfile-2f2ae.firebaseapp.com",
-  projectId: "uploadfile-2f2ae",
-  storageBucket: "uploadfile-2f2ae.appspot.com",
-  messagingSenderId: "134385431551",
-  appId: "1:134385431551:web:379bcb9abed8d365d66dca"
+  apiKey: process.env.APP_API_KEY,
+  authDomain: process.env.APP_AUTH_DOMAIN,
+  projectId: process.env.APP_PROJECT_ID,
+  storageBucket: process.env.APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.APP_MESSAGING_SENDER_ID,
+  appId: process.env.APP_APP_ID,
 };
 
 // Initialize Firebase
